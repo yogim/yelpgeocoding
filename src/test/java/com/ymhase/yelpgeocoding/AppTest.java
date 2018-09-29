870da185f003beda9c369cea3961e41deb5f5915
yogim <yogeshmhase89@gmail.com>
2018-09-29 13:59:29 -0700

    first commit

---
+++ src/test/java/com/ymhase/yelpgeocoding/AppTest.java
@@ -0,0 +1,20 @@
+package com.ymhase.yelpgeocoding;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
