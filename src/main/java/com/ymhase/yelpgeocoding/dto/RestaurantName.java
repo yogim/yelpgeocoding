b6e7f5d0675951c58fec2f46fa7ee7c11505ae03
yogim <yogeshmhase89@gmail.com>
2018-09-29 20:22:32 -0700

    properties, restcontroller for restaurant list added

---
+++ src/main/java/com/ymhase/yelpgeocoding/dto/RestaurantName.java
@@ -0,0 +1,16 @@
+package com.ymhase.yelpgeocoding.dto;
+
+public class RestaurantName {
+	
+	private String locationName;
+
+	public String getLocationName() {
+		return locationName;
+	}
+
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+	
+
+}
