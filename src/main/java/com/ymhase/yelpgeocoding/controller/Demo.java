bb950c039a98396e31f48da266a804ac145f5897
yogim <yogeshmhase89@gmail.com>
2018-09-29 21:42:31 -0700

    removed api key from demo class

--- src/main/java/com/ymhase/yelpgeocoding/controller/Demo.java
+++ src/main/java/com/ymhase/yelpgeocoding/controller/Demo.java
@@ -5,15 +5,16 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class Demo {
 
 	public static void main(String[] args) throws Exception {
-		URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=\"600 langsdorf drive fullerton\"");
+		URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=600 langsdorf drive fullerton&key=AIzaSyBsg3DmhAle-_G9X3Kh6Mgcqt_BlVldKmk");
 			
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
-		//conn.setRequestProperty("Accept", "application/json");
-		conn.setRequestProperty("x-api-key", "AIzaSyB4v-0YF79q5NZssShiFiiU76Ymc0z4QQk");
+		conn.setRequestProperty("Content-Type", "application/json");
 
 		if (conn.getResponseCode() != 200) {
 			throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
@@ -23,6 +24,7 @@ public class Demo {
 
 		String output;
 		System.out.println("Output from Server .... \n");
+		
 		while ((output = br.readLine()) != null) {
 			System.out.println(output);
 		}
--- src/main/java/com/ymhase/yelpgeocoding/controller/RestaurantController.java
+++ src/main/java/com/ymhase/yelpgeocoding/controller/RestaurantController.java
@@ -6,44 +6,35 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.ymhase.yelpgeocoding.dto.RestaurantName;
 
 //@RestController
 public class RestaurantController {
 
-	//@RequestMapping(method = RequestMethod.POST, value = "/restaurantlist")
+	// @RequestMapping(method = RequestMethod.POST, value = "/restaurantlist")
 	public void login(@RequestBody RestaurantName restaurantName) throws Exception {
-		
-		URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=\""+restaurantName.getLocationName()+"\"");
+
+		URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=\""
+				+ restaurantName.getLocationName() + "\"");
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 		conn.setRequestProperty("Accept", "application/json");
 		conn.setRequestProperty("x-api-key", "AIzaSyB4v-0YF79q5NZssShiFiiU76Ymc0z4QQk");
-		
+
 		if (conn.getResponseCode() != 200) {
-			throw new RuntimeException("Failed : HTTP error code : "
-					+ conn.getResponseCode());
+			throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
 		}
 
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-				(conn.getInputStream())));
+		BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
 
-			String output;
-			System.out.println("Output from Server .... \n");
-			while ((output = br.readLine()) != null) {
-				System.out.println(output);
-			}
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
 
-			conn.disconnect();
-
-			
-
-
+		conn.disconnect();
 
 	}
 
